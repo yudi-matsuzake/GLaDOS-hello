5a51c88b83f0b31d46081ba7ec2dd0e5926aaff3
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2016-02-19 13:00:41 -0200

    Beep adicionado

--- GLaDOS-hello.sh
+++ GLaDOS-hello.sh
@@ -5,10 +5,14 @@ portal(){
 }
 
 #sleep 10
-aplay sounds/GLaDOS_00_part1_entry-1.wav sounds/GLaDOS_00_part1_entry-7.wav &> /dev/null
+aplay sounds/GLaDOS_00_part1_entry-1.wav sounds/GLaDOS_00_part1_entry-7.wav &> /dev/null &
+echo "Hello and, again, welcome to the Aperture Science computer-aided enrichment center." | beep -cl 75 -f 100
+echo "Stand back. The portal will open in" | beep -cl 75 -f 100
+echo "three..." | beep -cl 75 -f 100 ; sleep 0.5
+echo "two..." | beep -cl 75 -f 100 ; sleep 0.5
+echo "one..." | beep -cl 75 -f 100
 
-#p=$(portal)
-
+wait
 cd portals
 ./portal
 #gnome-terminal --tab -e "/bin/bash -c 'cd portals/ ./portal;  exec /bin/bash -i'"
--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# GLaDOS-hello
+GLaDOS-hello
+------------
 Pequeno script com a temática do jogo Portal.
 
-# Dependências
+## Dependências
 
-* Pacote `xdotool`
+* `xdotool`
+
+* `libgtkmm-3.0`
+
+* `beep`
+
+## Compilação
+
+```shell
+$ cd portals
+$ make
+```
+
+## Execução
+
+```shell
+$ chmod +x GLaDOS-hello.sh
+$ ./GLaDOS-hello.sh
+```
